692a3b837ce5dba6cd2d85afe699fd904d2f1eb6
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-03-07 16:32:21 -0500

    first commit

---
+++ src/main/java/JavaSort.java
@@ -0,0 +1,62 @@
+//import java.text.Collator;
+//import java.util.*;
+//import java.util.Collections;
+//
+//class Student{
+//    private int id;
+//    private String fname;
+//    private double cgpa;
+//
+//    public Student(int id, String fname, double cgpa) {
+//        super();
+//        this.id = id;
+//        this.fname = fname;
+//        this.cgpa = cgpa;
+//    }
+//    public int getId() {
+//        return id;
+//    }
+//    public String getFname() {
+//        return fname;
+//    }
+//    public double getCgpa() {
+//        return cgpa;
+//    }
+//
+//}
+//
+//
+////Complete the code
+//public class Solution
+//{
+//    public static void main(String[] args){
+//        Scanner in = new Scanner(System.in);
+//        int testCases = Integer.parseInt(in.nextLine());
+//
+//        List<Student> studentList = new ArrayList<Student>();
+//        while(testCases>0){
+//            int id = in.nextInt();
+//            String fname = in.next();
+//            double cgpa = in.nextDouble();
+//
+//            //creating object
+//            Student st = new Student(id, fname, cgpa);
+//            studentList.add(st);
+//
+//            testCases--;
+//        }
+//
+//        //THIS COMPARES EVERYTHING
+//        Collections.sort(studentList, Comparator.comparing(Student :: getCgpa)
+//                .reversed().thenComparing(Student::getFname)
+//                .thenComparing(Student::getId));
+//
+//        //PRINT OUT THE LIST
+//        for(Student st: studentList){
+//            System.out.println(st.getFname());
+//        }
+//    }
+//}
+//
+//
+//
---
+++ src/main/java/MapPractice.java
@@ -0,0 +1,42 @@
+//import javax.swing.plaf.synth.SynthOptionPaneUI;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.Scanner;
+//
+//public class Solution {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter the amount of map entries: ");
+//        int n = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        for (int i = 0; i < ; i++) {
+//
+//        }
+//
+//        Map<String, Integer> map = new HashMap<String, Integer>();
+//
+//        for (int i = 0; i < n ; i++) {
+//            String name = scanner.nextLine();
+//            Integer age = scanner.nextInt();
+//            scanner.nextLine();
+//            map.put(name, age);
+//        }
+//
+//        for (Map.Entry<String, Integer> i : map.entrySet()) {
+//
+//            System.out.println( i.getKey() + ":" + i.getValue());
+//        }
+//    }
+//}
+//
+////3
+////John
+////35
+////Stefan
+////30
+////Lusy
+////60
